797f61b21b27d61d40a273e66827b82652bf50ac
grossmann94 <grossmann94@gmail.com>
2023-03-06 09:46:29 +0100

    added a formgroup

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
-  styleUrls: ['./profile-editor.component.css']
+  styleUrls: ['./profile-editor.component.css'],
 })
 export class ProfileEditorComponent {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 
+  onSubmit() {
+    console.warn(this.profileForm.value);
+    console.log(this.profileForm.valid);
+  }
 }
