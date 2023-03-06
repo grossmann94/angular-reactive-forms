020238ecd64163a425f1c8fe2a76618f4895fc01
grossmann94 <grossmann94@gmail.com>
2023-03-06 09:21:13 +0100

    added css and name editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NameEditorComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NameEditorComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
