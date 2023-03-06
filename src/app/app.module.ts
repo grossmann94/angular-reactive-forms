edbb2de68793359c67afc807ee2545ccd67006d5
grossmann94 <grossmann94@gmail.com>
2023-03-06 09:15:40 +0100

    generated name-editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameEditorComponent
   ],
   imports: [
     BrowserModule
